5198b2a8e541e15db1fcf481aa4f7450a78e1a34
Monika <monika.sanjoth@gmail.com>
2023-03-15 17:11:14 +0530

    commit

---
+++ Maven_Project_Assignment/src/test/java/Assignment.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		
+		driver.close();
+
+	}
+
+	}
+
+
